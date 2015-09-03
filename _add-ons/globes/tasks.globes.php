55c4a8a3237898ca33c1f60980403e287e4c7d70
Grant Rowley <durbanvisuals@gmail.com>
2015-09-03 14:47:36 -0600

    added error checking for global array key

--- _add-ons/globes/tasks.globes.php
+++ _add-ons/globes/tasks.globes.php
@@ -5,14 +5,20 @@ use Symfony\Component\Finder\Finder as Finder;
 class Tasks_globes extends Tasks {
 
 	public function getOverviewData() {
-		
+
 		$theme_settings = Config::getThemesPath() . '/' . Config::getTheme() . '/theme.yaml';
-		
+
 		$theme_yaml = File::get($theme_settings);
-		
+
 		$settings = YAML::parse($theme_yaml);
-		
-		$globals = $settings['global'];
+
+		if (array_key_exists('global', $settings)) {
+			$globals = $settings['global'];
+		} else {
+			$globals = array(
+				'error' => 'No global key was found in your theme.yaml file'
+			);
+		}
 
 		return compact('theme_settings', 'theme_yaml', 'globals');
 	}
--- _add-ons/globes/templates/globes-overview.php
+++ _add-ons/globes/templates/globes-overview.php
@@ -1,20 +1,32 @@
 <div class="container">
 	<form action="<?php echo $app->urlFor('globes') . '/update' ?>" method="post" class="primary-form">
 	<div class="section content">
-		
-		<?php 
-			foreach ($globals as $key => $setting) {
-				
-		?>
-			<div class="input-block input-text">
-				<label for="<?php echo $key; ?>"><?php echo Slug::prettify($key); ?></label><input type="text" name="vars['<?php echo $key; ?>']" value="<?php echo $setting; ?>">
-			</div>
-		<?php
-				
-			}
-		?>
-		
-		
+
+		<h1>
+			Site Globals
+		</h1>
+		<hr/>
+
+		<?php foreach ($globals as $key => $setting): ?>
+			<?php if ($key === 'error'): ?>
+				<p>
+					<b><?php echo $setting; ?></b>
+				</p>
+<pre>
+global:
+  variable: default value
+  - or -
+  variable_name:
+</pre>
+
+			<?php else: ?>
+				<div class="input-block input-text">
+					<label for="<?php echo $key; ?>"><?php echo Slug::prettify($key); ?></label><input type="text" name="vars['<?php echo $key; ?>']" value="<?php echo $setting; ?>">
+				</div>
+			<?php endif; ?>
+		<?php endforeach; ?>
+
+
 	</div>
 	<div id="publish-action" class="footer-controls push-down">
       <input type="submit" class="btn" value="Save &amp; Publish" id="publish-submit">
